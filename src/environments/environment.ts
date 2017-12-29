9156163bcc0ae0a8ff10b77bb608910d683563b5
Corentin <cbanzet@gmail.com>
2017-12-29 16:05:32 +0100

    remove firebaseConfig

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,6 @@
 export const environment = {
     production: false,
     firebaseConfig: {
-		    apiKey: "AIzaSyCbQJksrhQ8kBZCDGlmyzxoGAIXMmCbMic",
-		    authDomain: "david-lucas-paris-dev.firebaseapp.com",
-		    databaseURL: "https://david-lucas-paris-dev.firebaseio.com",
-		    projectId: "david-lucas-paris-dev",
-		    storageBucket: "david-lucas-paris-dev.appspot.com",
-		    messagingSenderId: "618080544800"
+
     }
 };
